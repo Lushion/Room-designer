56b8eda97ce035061ed8f51ac700c991756aab23
Kévin Phan <45756291+Luxyrion@users.noreply.github.com>
2020-07-06 18:29:12 +0900

    Base Code
    Rotation, Translation of Furnitures available.
    
    Use "<" and ">" to rotate the object and the directional arrows to translate the object.
    
    Use reset to reset the object to its initial position.

---
+++ Mixed Reality Room Project/Assets/Rotation.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    public GameObject furniture;
+    public int n = 0;
+    public float y;
+    public float rotationSpeed;
+
+    void Start()
+    {
+        y = 0.0f;
+        rotationSpeed = 5.0f;
+    }
+
+    void Update()
+    {
+        //if (rotationActivated == true)
+        //{
+        //    y = y + sign * rotationSpeed;
+        //}
+
+         
+        furniture.transform.localRotation = Quaternion.Euler(0, y, 0);
+    }
+
+
+    public void leftClick()
+    {
+        y += rotationSpeed;
+    }
+
+    public void rightClick()
+    {
+        y -= rotationSpeed;
+    }
+}
